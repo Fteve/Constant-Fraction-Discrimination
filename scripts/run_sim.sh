c75645228ccc8f9cee7058958ccba5a89f2257a1
CHENG <steven.cheng@psi.ch>
2026-03-23 17:36:05 +0100

    add simulation run script

---
+++ scripts/run_sim.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Stops the script if any command fails
+set -e
+
+echo "Activating virtual environment...
+"
+source venv/bin/activate
+
+cd ./Simulation
+
+echo "Checking and installing required packages...
+"
+pip install numpy scipy plotly dash
+echo "
+Package checks complete!
+"
+
+
+echo "Running simulation web app...
+"
+python cfd_app.py
+
